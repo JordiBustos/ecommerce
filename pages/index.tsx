f92ba16d32f5d87f4a5849e318acb47bac75bd54
JordiBustos <jordi.bustos193771@alumnos.info.unlp.edu.ar>
2023-04-23 00:09:19 -0300

    Search bar funcionallity, styling, etc

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,13 +22,27 @@ const sortingBasedOnFilters = {
 export default function IndexPage({ nodes, terms }) {
   const [selectedCategories, setSelectedCategories] = useState([]);
   const [selectedFilter, setSelectedFilter] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
 
   const handleSelectFilter = (event) => setSelectedFilter(event.target.value);
   const handleCategorySelect = (event) => {
     const category = event.target.value;
     event.target.checked
       ? setSelectedCategories([...selectedCategories, category])
-      : setSelectedCategories(selectedCategories.filter(c => c !== category));
+      : setSelectedCategories(selectedCategories.filter((c) => c !== category));
+  };
+
+  const handleSearchInputChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const filterSearchQuery = (nodes) => {
+    if (searchQuery !== "") {
+      nodes = nodes.filter((node) =>
+        node.title.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+    }
+    return nodes;
   };
 
   return (
@@ -41,7 +55,16 @@ export default function IndexPage({ nodes, terms }) {
         />
       </Head>
       <div>
-        <h1 className="mb-10 text-6xl font-black">Products</h1>
+        <div className="flex justify-between">
+          <h1 className="w-1/3 mb-10 text-6xl font-black">Productos</h1>
+          <input
+            type="text"
+            placeholder="Buscar"
+            value={searchQuery}
+            onChange={handleSearchInputChange}
+            className="p-1 mb-10 w-2/3 rounded shadow-md transition duration-300 ease-in-out hover:shadow-xl hover:border-transparent hover:ring-2 hover:ring-purple-600"
+          />
+        </div>
         <div id="filters" className="flex justify-between mb-10">
           {terms.map((category) => (
             <CheckboxItem
@@ -68,19 +91,21 @@ export default function IndexPage({ nodes, terms }) {
         </div>
         <div className="flex flex-wrap justify-between gap-1">
           {nodes?.length ? (
-            nodes.sort(sortingBasedOnFilters[selectedFilter]).map((node) =>
-              selectedCategories.includes(
-                terms[
-                  node.field_tipo.resourceIdObjMeta.drupal_internal__target_id -
-                    1
-                ].name
-              ) || selectedCategories.length == 0 ? (
-                <div className="basis-1/4 grow-0" key={node.id}>
-                  <NodeItemTeaser node={node} terms={terms} />
-                  <hr className="my-10" />
-                </div>
-              ) : null
-            )
+            filterSearchQuery(nodes)
+              .sort(sortingBasedOnFilters[selectedFilter])
+              .map((node) =>
+                selectedCategories.includes(
+                  terms[
+                    node.field_tipo.resourceIdObjMeta
+                      .drupal_internal__target_id - 1
+                  ].name
+                ) || selectedCategories.length == 0 ? (
+                  <div className="basis-1/4 grow-0" key={node.id}>
+                    <NodeItemTeaser node={node} terms={terms} />
+                    <hr className="my-10" />
+                  </div>
+                ) : null
+              )
           ) : (
             <p className="py-4">No nodes found</p>
           )}
